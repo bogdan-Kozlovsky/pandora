c22fa32aee42a4e64993309f2bdc900800e2a090
Bogdan <bogdankozlovski18@gmail.com>
2021-06-14 18:12:10 +0300

    scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,25 @@
 $(function () {
+  // плавний скрол
+  $(".menu a, .footer__logo").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+  // плавний скрол
+  $(window).scroll(function () {
+    if (this.scrollY > 500) {
+      $('.scroll-btn').addClass("show");
+    } else {
+      $('.scroll-btn').removeClass("show");
+    }
+  });
+  $('.scroll-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+    $('html').css("scrollBehavior", "auto");
+  });
+  // иконка наверх
+  // слайдер
   $('.studio__slider').slick({
     autoplay: true,
     autoplaySpeed: 3000,
@@ -22,8 +43,8 @@ $(function () {
     fade: true,
     prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrows-left.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrows-right.svg" alt=""></button>',
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-
+    autoplay: true,
+    autoplaySpeed: 3000,
   });
+  // слайдер
 });
