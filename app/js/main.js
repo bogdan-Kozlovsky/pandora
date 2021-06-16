6acc5f8361b6162de70d6fd0033008d8afc3e71c
Bogdan <bogdankozlovski18@gmail.com>
2021-06-16 07:34:32 +0300

    адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,58 @@
 $(function () {
+
+  // слайдер
+  $('.studio__slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrows-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrows-right.svg" alt=""></button>',
+  });
+  $('.slider__images-inner').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.slider__content-inner',
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 440,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
+  $('.slider__content-inner').slick({
+    asNavFor: '.slider__images-inner',
+    centerMode: true,
+    focusOnSelect: true,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrows-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrows-right.svg" alt=""></button>',
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+  // слайдер
   // плавний скрол
-  $(".menu a, .footer__logo").on("click", function (event) {
+  $(".menu a, .footer__content-link").on("click", function (event) {
     event.preventDefault();
     var id = $(this).attr('href'),
       top = $(id).offset().top;
@@ -19,32 +71,10 @@ $(function () {
     $('html').css("scrollBehavior", "auto");
   });
   // иконка наверх
-  // слайдер
-  $('.studio__slider').slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    fade: true,
-    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrows-left.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrows-right.svg" alt=""></button>',
+  // адаптивий бургер
+  $('.burger, .menu a').click(function () {
+    $('.menu__list').toggleClass("active");
+    $('.burger').toggleClass("active");
   });
-  $('.slider__images-inner').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    arrows: false,
-    asNavFor: '.slider__content-inner',
-    // centerMode: true,
-    // centerPadding: '0',
-    // cssEase: 'linear',
-  });
-  $('.slider__content-inner').slick({
-    asNavFor: '.slider__images-inner',
-    centerMode: true,
-    focusOnSelect: true,
-    fade: true,
-    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrows-left.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrows-right.svg" alt=""></button>',
-    autoplay: true,
-    autoplaySpeed: 3000,
-  });
-  // слайдер
+  // адаптивий бургер
 });
